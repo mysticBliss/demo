e4f679b54059e22e1871633df2dbcf7c18665212
Saqib Mujtaba Tamli (Industrial STAML Engineering Services (ISTAMLES)) <staml@wipro.com>
2019-02-04 13:23:02 +0530

    maps

--- t2k/static/js/mapmarker_func.jquery.js
+++ t2k/static/js/mapmarker_func.jquery.js
@@ -1,6 +1,6 @@
 		//set up markers
 		var myMarkers = {"markers": [
-				{"latitude": "34.0671741", "longitude":"74.7525609", "icon": "img/map-marker.png"}
+				{"latitude": "34.0671741", "longitude":"74.7525609", "icon": "/static/img/map-marker.png"}
 			]
 		};
 
